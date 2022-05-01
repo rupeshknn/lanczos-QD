e4ad78e2892c8c380dcbadbd8ea8ec865d6c3df6
rupeshknn <rupeshknn@gmail.com>
2022-05-01 23:42:23 +0530

    updated docstrings to numpy format

--- lanczos.py
+++ lanczos.py
@@ -8,20 +8,25 @@ from scipy.sparse import csr_matrix
 from helper import eigh_tridiagonal, construct_tridiag
 print(20)
 def lanczos_basis(array: csr_matrix, v_0: np.ndarray, k_dim: int) -> Tuple[np.ndarray, np.ndarray]:
-    '''
-    Finds the lowest k_dim eigen values and eigen vectors of alpha symmetric array
+    """
+    Tridigonalises krylov subspace of dimension k_dim for a given sparse array
 
-    array : ndarray, shape (ndim, ndim)
-    Array to diagnolize.
-    v_0 : ndarray, shape (ndim,)
-    A vector to start the lanczos iteration
-    k_dim : scalar
-    the dim of the krylov subspace
+    Parameters
+    ----------
+    array : csr_matrix
+        sparse array to diagonalise
+    v_0 : np.ndarray
+        inital vector
+    k_dim : int
+        dimension of the krylov subspace
 
-    returns
-    eigen_value : ndarray, shape (k_dim,) Eigenvalues
-    eigen_vectors : ndarray, shape (ndim, k_dim) Eigenvectors
-    '''
+    Returns
+    -------
+    Tridiagonal : np.ndarray
+        tridigonalised matrix
+    q_basis : np.ndarray
+        basis of the krylov subspace
+    """
     data_type = array.dtype
     v_0 = np.array(v_0).reshape(-1,1) # ket
     array_dim = array.shape[0]
@@ -63,6 +68,25 @@ def lanczos_basis(array: csr_matrix, v_0: np.ndarray, k_dim: int) -> Tuple[np.nd
     return Tridiagonal, q_basis
 
 def lanczos_eig(array: csr_matrix, v_0: np.ndarray, k_dim: int) -> Tuple[np.ndarray, np.ndarray]: 
+    """
+    Finds the lowest k_dim eigenvalues and corresponding eigenvectors of a hermitian array
+
+    Parameters
+    ----------
+    array : csr_matrix
+        sparse array to diagonalise
+    v_0 : np.ndarray
+        inital vector
+    k_dim : int
+        dimension of the krylov subspace
+    accurate : bool, optional
+        Additional steps to increase accuracy, by default False
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        Eigenvalues and Eigenvectors
+    """
     Tridiagonal, q_basis = lanczos_basis(array, v_0, k_dim)
     eigen_value, eigen_vectors_t = np.linalg.eigh(Tridiagonal)
 
